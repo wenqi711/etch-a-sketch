562507a8e14db759da1c35a1e197ceaa57cc0b7b
wenqi711 <152916174+wenqi711@users.noreply.github.com>
2025-12-18 19:33:43 +0400

    Set up grid

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const mainBody = document.querySelector("body");
-mainBody.textContent = "Blue";
+const pixelContainer = document.querySelector(".pixel-container");
+const numOfPixelsPerSide = 16;
+
+for (let i = 0; i < numOfPixelsPerSide; i++) {
+    const pixelRow = document.createElement("div");
+    pixelRow.classList.add("pixel-row");
+    pixelContainer.append(pixelRow);
+    for (let j = 0; j < numOfPixelsPerSide; j++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixelRow.append(pixel);
+    }
+}
