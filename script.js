9509569bff1d935f3f3072e3bd18a0376653b04d
wenqi711 <152916174+wenqi711@users.noreply.github.com>
2025-12-19 13:13:22 +0400

    Write code for adding new grid button

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const pixelContainer = document.querySelector(".pixel-container");
-const numOfPixelsPerSide = 16;
+let numOfPixelsPerSide = 16;
+setUpGrid(numOfPixelsPerSide);
 
-for (let i = 0; i < numOfPixelsPerSide; i++) {
-    const pixelRow = document.createElement("div");
-    pixelRow.classList.add("pixel-row");
-    pixelContainer.append(pixelRow);
-    for (let j = 0; j < numOfPixelsPerSide; j++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixelRow.append(pixel);
+function setUpGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const pixelRow = document.createElement("div");
+        pixelRow.classList.add("pixel-row");
+        pixelContainer.appendChild(pixelRow);
+        for (let j = 0; j < num; j++) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            pixelRow.appendChild(pixel);
+        }
+    }
+
+    const pixelList = document.querySelectorAll(".pixel");
+    for (let i = 0; i < pixelList.length; i++) {
+        pixelList[i].addEventListener("mouseenter", () => {
+            pixelList[i].classList.add("hover");
+        });
     }
 }
 
-const pixelList = document.querySelectorAll(".pixel");
-for (let i = 0; i < pixelList.length; i++) {
-    pixelList[i].addEventListener("mouseenter", () => {
-        pixelList[i].classList.add("hover");
-    });
-}
+const buttonContainer = document.querySelector(".button-container");
+
+const newGridButton = document.createElement("button");
+newGridButton.textContent = "New Grid";
+buttonContainer.appendChild(newGridButton);
+newGridButton.addEventListener("click", function () {
+    const userSelection = +(prompt("Enter a number between 3 and 100:", ""));
+    if (userSelection > 2 && userSelection < 101 && Math.floor(userSelection) === userSelection) {
+        numOfPixelsPerSide = userSelection;
+        document.querySelectorAll(".pixel-row").forEach(e => e.remove());
+        document.querySelectorAll(".pixel").forEach(e => e.remove());
+        setUpGrid(numOfPixelsPerSide);
+    } else {
+        alert("This is not a valid input. Please select a number between 3 and 100.");
+    }
+})
